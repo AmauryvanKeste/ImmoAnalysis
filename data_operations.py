e3665ca45335507b298ed3bae873fa8b7efca7ba
Roldan87 <jose.roldan1987@gmail.com>
2021-06-28 14:27:38 +0200

    data ops commands

--- data_operations.py
+++ data_operations.py
@@ -4,21 +4,20 @@
 
 # rename columns:
     '''
-    df_houses = df_houses.rename(columns={ "Area [m²]" : "area",
-      ,"Price [€]" : "price
-      ,"state of the building": "building_state
-      ,"number of facades": "facades
-      ,"number of bedrooms": "bedrooms
-      ,"fully equipped kitchen": "kitchen_equipped
-      ,"open fire": "open_fire
-      ,"locality [zip code]": "locality
-      ,"surface of the land [m²]": "land_surface
-      ,"terrace surface [m²]": "terrace_surface
-      ,"swimming pool": "swimming_pool
-      ,"type of property": "property_type
-      ,"subtype of property": "property_subtype
-      ,"garden surface [m²]": "garden_surface
-    }
+    df_houses = df_houses.rename(columns={"Area [m²]" : "area"
+      ,"Price [€]" : "price"
+      ,"state of the building": "building_state"
+      ,"number of facades": "facades"
+      ,"number of bedrooms": "bedrooms"
+      ,"fully equipped kitchen": "kitchen_equipped"
+      ,"open fire": "open_fire"
+      ,"locality [zip code]": "locality"
+      ,"surface of the land [m²]": "land_surface"
+      ,"terrace surface [m²]": "terrace_surface"
+      ,"swimming pool": "swimming_pool"
+      ,"type of property": "property_type"
+      ,"subtype of property": "property_subtype"
+      ,"garden surface [m²]": "garden_surface"}
 '''
 # fix typo:
     #df_houses['property_subtype'] = df_houses['property_subtype'].replace(to_replace='exceptiona', value='exeptional')
