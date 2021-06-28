ea9faa05ff40b3b647fb9250074e66c0b23fd716
dadude <dadude@users.noreply.github.com>
2021-06-28 15:50:39 +0200

    prepping for categoricals2

--- data_operations.py
+++ data_operations.py
@@ -67,13 +67,13 @@ def main():
     for column in change_nan_to_undefined:
         replace_nan_in_column(df_houses, column, "undefined")
 
-    kitchen_category = ['installed', 'undefined', 'hyper equipped', 'semi equipped', 'usa semi equipped',
+    kitchen_cat = ['installed', 'undefined', 'hyper equipped', 'semi equipped', 'usa semi equipped',
                         'usa installed', 'usa hyper equipped', 'not installed' 'usa uninstalled']
-    building_state = ['good', 'just renovated', 'as new', 'to renovate', 'to be done up', 'to restore',
+    building_state_cat = ['good', 'just renovated', 'as new', 'to renovate', 'to be done up', 'to restore',
                        'undefined']
-    property_type = ['house', 'land', 'other' ]
-    property_subtype = ['house', 'villa', 'mixed', 'town', 'farmhouse', 'chalet', 'country', 'exceptional', 'building',
-                        'apartment', 'mansion', 'bungalow', 'other', 'manor', 'castle', 'land']
+    property_type_cat = ['house', 'land', 'other' ]
+    property_subtype_cat = ['house', 'villa', 'mixed', 'town', 'farmhouse', 'chalet', 'country', 'exceptional',
+                            'building', 'apartment', 'mansion', 'bungalow', 'other', 'manor', 'castle', 'land']
 
 if __name__ == '__main__':
     main()
